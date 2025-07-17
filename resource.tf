962eb2f3674f4ca3b16a8ce8f3090ff9b3894968
BhavaniN13 <bhavanin.n6@gmail.com>
2025-07-17 11:46:09 +0000

    container

--- resource.tf
+++ resource.tf
@@ -1,7 +1,10 @@
+#resource group 
 resource "azurerm_resource_group" "terraAzure" {
     name     = "Casestudy1"
     location = "East US"
 }
+
+#storage Account 
 resource "azurerm_storage_account" "terraStorage" {
   name                     = "casestudy1storage13"   
   resource_group_name      = azurerm_resource_group.terraAzure.name
@@ -13,3 +16,18 @@ resource "azurerm_storage_account" "terraStorage" {
     environment = "CaseStudy"
   }
 }
+
+
+# blob container 
+# Blob Container
+resource "azurerm_storage_container" "terraContainer" {
+  name                  = "casestudycontainer13"
+  storage_account_name  = azurerm_storage_account.terraStorage.name
+  container_access_type = "private"  # options: "private", "blob", or "container"
+
+  metadata = {
+    environment = "CaseStudy"
+  }
+}
+
+
