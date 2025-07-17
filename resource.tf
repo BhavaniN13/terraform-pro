e7151ce48636cd92f52f077f76f7ef2fd93b3521
BhavaniN13 <bhavanin.n6@gmail.com>
2025-07-17 11:32:09 +0000

    sa

--- resource.tf
+++ resource.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "terraAzure" {
     name     = "Casestudy1"
     location = "East US"
 }
+resource "azurerm_storage_account" "terraStorage" {
+  name                     = "casestudy1storage"    # must be globally unique
+  resource_group_name      = azurerm_resource_group.terraAzure.name
+  location                 = azurerm_resource_group.terraAzure.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "CaseStudy"
+  }
+}
